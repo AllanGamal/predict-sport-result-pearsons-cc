384f18d4d009c8ccca75f403b104d2d01bc277aa
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2022-06-06 20:50:51 +0200

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -1,5 +1,28 @@
 # check how many times a class of html tag with the tag value is found in the html
-def check_html_tag_count(url, element, tag, tag_value):
+# function that checks a website for a specific class of html tag
+
+# find the player stats table for the specific team
+from calendar import c
+
+
+def team_stats_table(tags, team):# find the spicific player stats table for the specific team
+    table_tag = ''
+    
+    for tag in tags: 
+        if team + ' Player Stats Table' in str(tag): 
+            table_tag = tag
+            print(table_tag)
+            break
+    return table_tag
+
+def n_rows(children): # find the number of rows in the table
+    count = 0
+    for child in children:
+        count += 1
+    return count
+    
+
+def check_html_tag(url, element, tag, team, row):
     import requests
     from bs4 import BeautifulSoup
     # get the html
@@ -7,10 +30,34 @@ def check_html_tag_count(url, element, tag, tag_value):
     # parse the html
     soup = BeautifulSoup(html.text, 'html.parser')
     # find the tag
-    tag = soup.find_all(element, {tag: tag_value})
-    # return the tag
-    return len(tag)
+    tags = soup.findAll(element)
+    
+    # find the player stats table for the specific team
+    caption_tag = team_stats_table(tags, team) 
+    table_tag = caption_tag.findNext('tbody') 
+    
+
+    player = table_tag.findChildren("tr")
+    player = player[row:row+1]
+    n_players = n_rows(player) # find the number of players in the table of a team
+    # go to the third row of children
+
+    minutes = int(soup.select('td[data-stat="minutes"]')[row].text) # find the minutes of a player
+    name = soup.select('th[csk]')[row].get('csk') # find the name of a player
+
+    
+    
+    if minutes < 75:
+        # return the tag value of children
+        return name
+        
+    else:
+        return 0
+    
 
 url = 'https://fbref.com/en/matches/81074285/Hellas-Verona-Sassuolo-August-21-2021-Serie-A'
-test = check_html_tag_count('https://fbref.com/en/matches/81074285/Hellas-Verona-Sassuolo-August-21-2021-Serie-A', 'tr', 'data-row', '0')
-print(test)
+
+tag_string = check_html_tag(url, 'caption', 'data-label', 'Hellas Verona', 0)
+print(tag_string)
+
+
