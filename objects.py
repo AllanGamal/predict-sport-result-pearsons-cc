841d17e074384e3833a16d6317b7acc21dc410b5
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2022-06-07 17:37:17 +0200

    obj functionality(not done), webscape attr and attr-value

--- objects.py
+++ objects.py
@@ -1,8 +1,142 @@
-class player: 
-    def __init__(self, name, team, goals, assists, pens_made, pens_att, shots_total, shots_on_target, cards_yellow, cards_red, touches, pressured, tackles, interceptions, blocks, xg, npxg, xa, sca, gca, passes_completed, passes, passes_pct, progressive_passes, carries, progressive_carries, dribbles_completed, dribbles, passes_total_distance, passes_progressive_distance, passes_completed_short, passes_short, passes_pct_short, passes_medium, passes_pct_medium, passes_completed_medium, passes_completed_long, passes_long, passes_pct_long, assisted_shots, passes_into_final_third, passes_into_penalty_area, crosses_into_penalty_area):
+class Player: 
+    # initialize the player class
+    def __init__(self, name):#, goals, assists, pens_made, pens_att, shots_total, shots_on_target, cards_yellow, cards_red, touches, pressures, tackles, interceptions, blocks, xg, npxg, xa, sca, gca, passes_completed, passes, passes_pct, progressive_passes, carries, progressive_carries, dribbles_completed, dribbles, passes_total_distance, passes_progressive_distance, passes_completed_short, passes_short, passes_pct_short, passes_medium, passes_pct_medium, passes_completed_medium, passes_completed_long, passes_long, passes_pct_long, assisted_shots, passes_into_final_third, passes_into_penalty_area, crosses_into_penalty_area, passes_live, passes_dead, passes_free_kicks, through_balls, passes_pressure, passes_switches, crosses, corner_kicks, corner_kicks_in, corner_kicks_out, corner_kicks_straight, passes_ground, passes_low, passes_high, passes_left_foot, passes_right_foot, passes_head, throw_ins, passes_offsides, passes_oob, passes_intercepted, passes_blocked, tackles_won, tackles_def_3rd, tackles_mid_3rd, tackles_att_3rd, dribble_tackles, dribbles_vs, dribble_tackles_pct, dribbled_past, pressure_regains, pressure_regain_pct, pressures_def_3rd, pressures_mid_3rd, pressures_att_3rd, blocked_shots, blocked_shots_saves, blocked_passes, tackles_interceptions, clearances, errors, touches_def_pen_area, touches_def_3rd, touches_mid_3rd, touches_att_3rd, touches_att_pen_area, touches_live_ball, dribbles_completed_pct, players_dribbled_past, nutmegs, carry_distance, carry_progressive_distance, carries_into_final_third, carries_into_penalty_area, miscontrols, dispossessed, pass_targets, passes_received, passes_received_pct, progressive_passes_received, cards_yellow_red, fouls, fouled, offsides, pens_won, pens_conceded, own_goals, ball_recoveries, aerials_won, aerials_lost, aerials_won_pct):
         self.name = name
-        self.team = team
+        #self.team = team
+        '''
+        self.goals = goals
+        self.assists = assists
+        self.pens_made = pens_made
+        self.pens_att = pens_att
+        self.shots_total = shots_total
+        self.shots_on_target = shots_on_target
+        self.cards_yellow = cards_yellow
+        self.cards_red = cards_red
+        self.touches = touches
+        self.pressures = pressures
+        self.tackles = tackles
+        self.interceptions = interceptions
+        self.blocks = blocks
+        self.xg = xg
+        self.npxg = npxg
+        self.xa = xa
+        self.sca = sca
+        self.gca = gca
+        self.passes_completed = passes_completed
+        self.passes = passes
+        self.passes_pct = passes_pct
+        self.progressive_passes = progressive_passes
+        self.carries = carries
+        self.progressive_carries = progressive_carries
+        self.dribbles_completed = dribbles_completed
+        self.dribbles = dribbles
+        self.passes_total_distance = passes_total_distance
+        self.passes_progressive_distance = passes_progressive_distance
+        self.passes_completed_short = passes_completed_short
+        self.passes_short = passes_short
+        self.passes_pct_short = passes_pct_short
+        self.passes_medium = passes_medium
+        self.passes_pct_medium = passes_pct_medium
+        self.passes_completed_medium = passes_completed_medium
+        self.passes_completed_long = passes_completed_long
+        self.passes_long = passes_long
+        self.passes_pct_long = passes_pct_long
+        self.assisted_shots = assisted_shots
+        self.passes_into_final_third = passes_into_final_third
+        self.passes_into_penalty_area = passes_into_penalty_area    
+        self.passes_live = passes_live
+        self.passes_dead = passes_dead
+        self.passes_free_kicks = passes_free_kicks
+        self.through_balls = through_balls
+        self.passes_pressure = passes_pressure
+        self.passes_switches = passes_switches
+        self.crosses = crosses
+        self.corner_kicks = corner_kicks
+        self.corner_kicks_in = corner_kicks_in
+        self.corner_kicks_out = corner_kicks_out
+        self.corner_kicks_straight = corner_kicks_straight
+        self.passes_ground = passes_ground
+        self.passes_low = passes_low
+        self.passes_high = passes_high
+        self.passes_left_foot = passes_left_foot
+        self.passes_right_foot = passes_right_foot
+        self.passes_head = passes_head
+        self.throw_ins = throw_ins
+        self.passes_offsides = passes_offsides
+        self.passes_oob = passes_oob
+        self.passes_intercepted = passes_intercepted
+        self.passes_blocked = passes_blocked
+        self.tackles_won = tackles_won
+        self.tackles_def_3rd = tackles_def_3rd
+        self.tackles_mid_3rd = tackles_mid_3rd
+        self.tackles_att_3rd = tackles_att_3rd
+        self.dribble_tackles = dribble_tackles
+        self.dribbles_vs = dribbles_vs
+        self.dribble_tackles_pct = dribble_tackles_pct
+        self.dribbled_past = dribbled_past
+        self.pressures = pressures
+        self.pressure_regains = pressure_regains
+        self.pressure_regain_pct = pressure_regain_pct
+        self.pressures_def_3rd = pressures_def_3rd
+        self.pressures_mid_3rd = pressures_mid_3rd
+        self.pressures_att_3rd = pressures_att_3rd
+        self.blocked_shots = blocked_shots
+        self.blocked_shots_saves = blocked_shots_saves
+        self.blocked_passes = blocked_passes
+        self.tackles_interceptions = tackles_interceptions
+        self.clearances = clearances
+        self.errors = errors
+        self.crosses_into_penalty_area = crosses_into_penalty_area
+        self.touches_def_pen_area = touches_def_pen_area
+        self.touches_def_3rd = touches_def_3rd
+        self.touches_mid_3rd = touches_mid_3rd
+        self.touches_att_3rd = touches_att_3rd
+        self.touches_att_pen_area = touches_att_pen_area
+        self.touches_live_ball = touches_live_ball
+        self.dribbles_completed_pct = dribbles_completed_pct
+        self.players_dribbled_past = players_dribbled_past
+        self.nutmegs = nutmegs
+        self.carry_distance = carry_distance
+        self.carry_progressive_distance = carry_progressive_distance
+        self.carries_into_final_third = carries_into_final_third
+        self.carries_into_penalty_area = carries_into_penalty_area
+        self.miscontrols = miscontrols
+        self.dispossessed = dispossessed
+        self.pass_targets = pass_targets
+        self.passes_received = passes_received
+        self.passes_received_pct = passes_received_pct
+        self.progressive_passes_received = progressive_passes_received
+        self.cards_yellow_red = cards_yellow_red
+        self.fouls = fouls
+        self.fouled = fouled
+        self.offsides = offsides
+        self.pens_won = pens_won
+        self.pens_conceded = pens_conceded
+        self.own_goals = own_goals
+        self.ball_recoveries = ball_recoveries
+        self.aerials_won = aerials_won
+        self.aerials_lost = aerials_lost
+        self.aerials_won_pct = aerials_won_pct
+'''
+    # add attributes to the class
+
+play_att_list = ['name', 'team', 'goals', 'assists', 'pens_made', 'pens_att', 'shots_total', 'shots_on_target', 'cards_yellow', 'cards_red', 'touches', 'pressures', 'tackles', 'interceptions', 'blocks', 'xg', 'npxg', 'xa', 'sca', 'gca', 'passes_completed', 'passes', 'passes_pct', 'progressive_passes', 'carries', 'progressive_carries', 'dribbles_completed', 'dribbles', 'passes_total_distance', 'passes_progressive_distance', 'passes_completed_short', 'passes_short', 'passes_pct_short', 'passes_medium', 'passes_pct_medium', 'passes_completed_medium', 'passes_completed_long', 'passes_long', 'passes_pct_long', 'assisted_shots', 'passes_into_final_third', 'passes_into_penalty_area', 'crosses_into_penalty_area', 'passes_live', 'passes_dead', 'passes_free_kicks', 'through_balls', 'passes_pressure', 'passes_switches', 'crosses', 'corner_kicks', 'corner_kicks_in', 'corner_kicks_out', 'corner_kicks_straight', 'passes_ground', 'passes_low', 'passes_high', 'passes_left_foot', 'passes_right_foot', 'passes_head', 'throw_ins', 'passes_offsides', 'passes_oob', 'passes_intercepted', 'passes_blocked', 'tackles_won', 'tackles_def_3rd', 'tackles_mid_3rd', 'tackles_att_3rd', 'dribble_tackles', 'dribbles_vs', 'dribble_tackles_pct', 'dribbled_past', 'pressure_regains', 'pressure_regain_pct', 'pressures_def_3rd', 'pressures_mid_3rd', 'pressures_att_3rd', 'blocked_shots', 'blocked_shots_saves', 'blocked_passes', 'tackles_interceptions', 'clearances', 'errors', 'touches_def_pen_area', 'touches_def_3rd', 'touches_mid_3rd', 'touches_att_3rd', 'touches_att_pen_area', 'touches_live_ball', 'dribbles_completed_pct', 'players_dribbled_past', 'nutmegs', 'carry_distance', 'carry_progressive_distance', 'carries_into_final_third', 'carries_into_penalty_area', 'miscontrols', 'dispossessed', 'pass_targets', 'passes_received', 'passes_received_pct', 'progressive_passes_received', 'cards_yellow_red', 'fouls', 'fouled', 'offsides', 'pens_won', 'pens_conceded', 'own_goals', 'ball_recoveries', 'aerials_won', 'aerials_lost', 'aerials_won_pct']
+
+# function to make a list of strings of every word in a string while ignoring every comma
+def get_words(string):
+    words = []
+    word = ""
+    for char in string:
+        if char == " ":
+            words.append(word)
+            word = ""
+        elif char == ",":
+            pass
+        else:
+            word += char
+    words.append(word)
+    return words
 
 
+# print(play_att_list)
 
-        
--- webscrape.py
+++ webscrape.py
@@ -1,5 +1,10 @@
 # check how many times a class of html tag with the tag value is found in the html
 # function that checks a website for a specific class of html tag
+import requests
+from bs4 import BeautifulSoup
+# connect another py file to this one
+# import objects as obj
+import objects as obj 
 
 # find the player stats table for the specific team
 from calendar import c
@@ -11,7 +16,6 @@ def team_stats_table(tags, team):# find the spicific player stats table for the 
     for tag in tags: 
         if team + ' Player Stats Table' in str(tag): 
             table_tag = tag
-            print(table_tag)
             break
     return table_tag
 
@@ -23,8 +27,7 @@ def n_rows(children): # find the number of rows in the table
     
 
 def check_html_tag(url, element, tag, team, row):
-    import requests
-    from bs4 import BeautifulSoup
+    
     # get the html
     html = requests.get(url)
     # parse the html
@@ -35,21 +38,21 @@ def check_html_tag(url, element, tag, team, row):
     # find the player stats table for the specific team
     caption_tag = team_stats_table(tags, team) 
     table_tag = caption_tag.findNext('tbody') 
-    
 
     player = table_tag.findChildren("tr")
     player = player[row:row+1]
+    
     n_players = n_rows(player) # find the number of players in the table of a team
     # go to the third row of children
 
     minutes = int(soup.select('td[data-stat="minutes"]')[row].text) # find the minutes of a player
     name = soup.select('th[csk]')[row].get('csk') # find the name of a player
 
-    
+    player_stats = soup.findAll("th", {"csk": name})
     
     if minutes < 75:
         # return the tag value of children
-        return name
+        return player_stats
         
     else:
         return 0
@@ -57,7 +60,66 @@ def check_html_tag(url, element, tag, team, row):
 
 url = 'https://fbref.com/en/matches/81074285/Hellas-Verona-Sassuolo-August-21-2021-Serie-A'
 
-tag_string = check_html_tag(url, 'caption', 'data-label', 'Hellas Verona', 0)
-print(tag_string)
+player_stats = check_html_tag(url, 'caption', 'NONE', 'Hellas Verona', 0)
+play_att_list = ['name', 'team', 'goals', 'assists', 'pens_made', 'pens_att', 'shots_total', 'shots_on_target', 'cards_yellow', 'cards_red', 'touches', 'pressures', 'tackles', 'interceptions', 'blocks', 'xg', 'npxg', 'xa', 'sca', 'gca', 'passes_completed', 'passes', 'passes_pct', 'progressive_passes', 'carries', 'progressive_carries', 'dribbles_completed', 'dribbles', 'passes_total_distance', 'passes_progressive_distance', 'passes_completed_short', 'passes_short', 'passes_pct_short', 'passes_medium', 'passes_pct_medium', 'passes_completed_medium', 'passes_completed_long', 'passes_long', 'passes_pct_long', 'assisted_shots', 'passes_into_final_third', 'passes_into_penalty_area', 'crosses_into_penalty_area', 'passes_live', 'passes_dead', 'passes_free_kicks', 'through_balls', 'passes_pressure', 'passes_switches', 'crosses', 'corner_kicks', 'corner_kicks_in', 'corner_kicks_out', 'corner_kicks_straight', 'passes_ground', 'passes_low', 'passes_high', 'passes_left_foot', 'passes_right_foot', 'passes_head', 'throw_ins', 'passes_offsides', 'passes_oob', 'passes_intercepted', 'passes_blocked', 'tackles_won', 'tackles_def_3rd', 'tackles_mid_3rd', 'tackles_att_3rd', 'dribble_tackles', 'dribbles_vs', 'dribble_tackles_pct', 'dribbled_past', 'pressure_regains', 'pressure_regain_pct', 'pressures_def_3rd', 'pressures_mid_3rd', 'pressures_att_3rd', 'blocked_shots', 'blocked_shots_saves', 'blocked_passes', 'tackles_interceptions', 'clearances', 'errors', 'touches_def_pen_area', 'touches_def_3rd', 'touches_mid_3rd', 'touches_att_3rd', 'touches_att_pen_area', 'touches_live_ball', 'dribbles_completed_pct', 'players_dribbled_past', 'nutmegs', 'carry_distance', 'carry_progressive_distance', 'carries_into_final_third', 'carries_into_penalty_area', 'miscontrols', 'dispossessed', 'pass_targets', 'passes_received', 'passes_received_pct', 'progressive_passes_received', 'cards_yellow_red', 'fouls', 'fouled', 'offsides', 'pens_won', 'pens_conceded', 'own_goals', 'ball_recoveries', 'aerials_won', 'aerials_lost', 'aerials_won_pct']
+
+# make a function that removes every double and single quotation mark from the string and backslashes from the string
+
+def add_object(stats_tag, list): 
+    for stat in stats_tag:
+        # make an object from webscrape.py
+        raw_stats = stat.parent
+        name = raw_stats.find('th').get('csk')
+        p1 = obj.Player(name)
+        print(p1.name)
+        break
+        
+    
+    for stat_tag in stats_tag:
+        raw_stats = stat_tag.parent
+        for att in reversed(list):
+            if att in str(raw_stats):
+                att_stat = raw_stats.find_all_next('td', {"data-stat": att})[0].text
+                if att_stat == '':
+                    att_stat = 0
+                att_stat = float(att_stat)
+                p1.att = att_stat
+                print(p1.name)
+                print(att)
+                print(p1.att)
+                print('\n')
+
+add_object(player_stats, play_att_list)
+                
+            
+        
+    
+'''   
+for volume in player_stats:
+    test = volume.parent
+    for testy in play_att_list:
+        
+            if testy in str(test):
+                play_att_list.remove(testy)
+    
+    if "crosses" in str(test):
+        abc = float(test.find_all('td', {"data-stat": "crosses"})[0:999999].text)
+        print(abc)
+'''
+    
 
 
+
+
+# function that checks how many times a class of html tag with the tag value is found in the html
+def check_html_tag_n(url, element, tag, team, row):
+    # get the html
+    html = requests.get(url)
+    # parse the html
+    soup = BeautifulSoup(html.text, 'html.parser')
+    # find the tag
+    tags = soup.findAll(element)
+    # find the player stats table for the specific team
+    caption_tag = team_stats_table(tags, team) 
+    table_tag = caption_tag.findNext('tbody') 
+    # go to the third row of children
